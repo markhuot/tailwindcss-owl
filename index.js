9726729c38579400d462ce8e1eeaa4c5a4cedbee
Christopher Kennedy <chris@onebrightlight.com>
2019-08-22 10:59:26 -0700

    Modify plugin to allow the use of variants
    Establishes a theme name of `cssOwls` to use in the config. By default, the plugin's theme uses the `responsive` variant, but will accept others, as well.

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
-module.exports = function({ e, addUtilities, config }) {
-  let newUtilities = {}
-  let margins = config('theme.margin', {})
-  
-  for (const key in margins) {
-    let className = `.${e(`o-${key}`)} > * + *`
-    let marginTop = margins[key]
+module.exports = function() {
+  return ({ theme, variants, e, config, addUtilities }) => {
+    const defaultCssOwlsTheme = {};
+    const defaultCssOwlsVariants = ["responsive"];
 
-    if (marginTop == 'auto') {
-      continue
+    const cssOwlsTheme = theme("cssOwls", defaultCssOwlsTheme);
+    const cssOwlVariants = variants("cssOwls", defaultCssOwlsVariants);
+
+    let cssOwlUtilities = {};
+    let margins = config("theme.margin", {});
+
+    for (const key in margins) {
+      let className = `.${e(`o-${key}`)} > * + *`;
+      let marginTop = margins[key];
+
+      if (marginTop == "auto") {
+        continue;
+      }
+
+      cssOwlUtilities[className] = { marginTop };
     }
 
-    newUtilities[className] = {marginTop}
-  }
-
-  addUtilities(newUtilities)
-}
+    addUtilities(cssOwlUtilities, cssOwlVariants);
+  };
+};
